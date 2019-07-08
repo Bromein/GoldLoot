860eb6492bfad89ebbb4ed3c19dcbf31d51e99fa
AJ Broman <ajbroman@gmail.com>
2019-07-07 21:07:00 -0400

    removed logged from prod build

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,12 @@ import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
 // ** utilizing an array here for MW so later I can easily spread into applyFn()
-const middlewares = [logger];
+const middlewares = [];
+
+//allows us to check what process environment is in
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
